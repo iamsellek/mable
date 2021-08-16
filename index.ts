00678a0db95e07f8c1651a9637a8a9fde1d5b045
Chris Sellek <iamsellek@gmail.com>
2021-08-16 09:04:55 -0400

    Add filter, find, and findAll helper functions. Add a type for a generic object.

--- index.ts
+++ index.ts
@@ -1,41 +1,15 @@
-import { BooleanCallback, ReturnGenericCallback, VoidCallback } from './types';
+import {
+  BooleanCallback,
+  GenericObject,
+  ReturnGenericCallback,
+  VoidCallback,
+} from './types';
 
 class MableObject<T> {
-  generic: Record<string, T>; // <- The Object
+  public theObject: GenericObject<T>; // <- "The Object"
 
-  constructor(generic: Record<string, T>) {
-    this.generic = generic;
-  }
-
-  /**
-   * Loops through each value on The Object and performs the logic
-   * in the callback function on each one.
-   *
-   * Syntactical sugar for Object.values(TheObject).forEach(callback);
-   */
-  forEach(callback: VoidCallback<T>): void {
-    Object.values(this.generic).forEach(callback);
-  }
-
-  /**
-   * Loops through each value on The Object. Makes a change to each value
-   * based on the logic in the callback.
-   *
-   * @param callback A callback that will take an item of type T, change
-   * it in some way, and return the changed value, which is also of type
-   * T.
-   *
-   * @returns a brand new MableObject with the changed values.
-   */
-
-  map(callback: ReturnGenericCallback<T>): MableObject<T> {
-    const newValues: Record<string, T> = {};
-
-    Object.keys(this.generic).forEach((key, index) => {
-      newValues[key] = callback(this.generic[key], index);
-    });
-
-    return new MableObject(newValues);
+  constructor(generic: GenericObject<T>) {
+    this.theObject = generic;
   }
 
   /**
@@ -54,6 +28,91 @@ class MableObject<T> {
     });
 
     return true;
+  }
+
+  /**
+   * Returns a new MableObject containing only the items that pass the
+   * conditional provided by the callback.
+   */
+  filter(callback: BooleanCallback<T>): MableObject<T> {
+    const newValues: GenericObject<T> = {};
+
+    Object.keys(this.theObject).forEach((key, index) => {
+      const item = this.theObject[key];
+
+      if (callback(item, index)) {
+        newValues[key] = item;
+      }
+    });
+
+    return new MableObject(newValues);
+  }
+
+  /**
+   * Returns the first value in The Object that matches the conditional
+   * provided by the callback or undefined if no matches are found.
+   */
+  find(callback: BooleanCallback<T>): T | undefined {
+    this.forEach((item, index) => {
+      if (callback(item, index)) {
+        return item;
+      }
+    });
+
+    return undefined;
+  }
+
+  /**
+   * Returns ALL values in The Object that match the conditional provided
+   * by the callback or undefined if no matches are found.
+   */
+  findAll(callback: BooleanCallback<T>): MableObject<T> | undefined {
+    const foundValues: GenericObject<T> = {};
+
+    Object.keys(this.theObject).forEach((key, index) => {
+      const item = this.theObject[key];
+
+      if (callback(item, index)) {
+        foundValues[key] = item;
+      }
+    });
+
+    if (Object.keys(foundValues).length === 0) {
+      return undefined;
+    } else {
+      return new MableObject(foundValues);
+    }
+  }
+
+  /**
+   * Loops through each value on The Object and performs the logic
+   * in the callback function on each one.
+   *
+   * Syntactical sugar for Object.values(TheObject).forEach(callback);
+   */
+  forEach(callback: VoidCallback<T>): void {
+    Object.values(this.theObject).forEach(callback);
+  }
+
+  /**
+   * Loops through each value on The Object. Makes a change to each value
+   * based on the logic in the callback.
+   *
+   * @param callback A callback that will take an item of type T, change
+   * it in some way, and return the changed value, which is also of type
+   * T.
+   *
+   * @returns a brand new MableObject with the changed values.
+   */
+
+  map(callback: ReturnGenericCallback<T>): MableObject<T> {
+    const newValues: GenericObject<T> = {};
+
+    Object.keys(this.theObject).forEach((key, index) => {
+      newValues[key] = callback(this.theObject[key], index);
+    });
+
+    return new MableObject(newValues);
   }
 }
 
--- types.ts
+++ types.ts
@@ -1,3 +1,5 @@
+export type GenericObject<T> = Record<string | number, T>;
+
 export type VoidCallback<T> = (item: T, index: number) => void;
 export type ReturnGenericCallback<T> = (item: T, index: number) => T;
 export type BooleanCallback<T> = (item: T, index: number) => boolean;
