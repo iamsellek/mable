c585fd90a7ce4666c80a5fbb8eedcc1fdebda77b
Chris Sellek <iamsellek@gmail.com>
2021-09-22 13:52:14 -0400

    Update dependencies and jest config.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   globals: {
     'ts-jest': {
-      tsConfig: 'tsconfig.json',
+      tsconfig: 'tsconfig.json',
     },
   },
   roots: ['<rootDir>/src'],
@@ -12,4 +12,6 @@ module.exports = {
   coverageReporters: ['cobertura', 'html'],
   testPathIgnorePatterns: ['/node_modules'],
   moduleDirectories: ['node_modules', '<rootDir>'],
+  transformIgnorePatterns: ['node_modules'],
+  preset: 'ts-jest',
 };
